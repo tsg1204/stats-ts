dd7bce8178e3754b61ec8eee46fc4d30e28a12b4
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-17 11:47:18 -0500

    add static methods to summary and reader

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,12 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
-import { HtmlReport } from './reportTargets/HtmlReport';
 
+//const csvFileReader = new CsvFileReader('football.csv');
 
-const csvFileReader = new CsvFileReader('football.csv');
+//const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv')
+const summary = Summary.winsAnalysisWithHtmlReport('Man United')
 
-const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new HtmlReport()
-  //new ConsoleReport()
-)
 
 summary.buildAndPrintReport(matchReader.matches);
